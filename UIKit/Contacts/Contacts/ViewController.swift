ea2a5051bd215a40a3344d7ebfb088b282d9b01f
trumanfromkorea <jangjh971008@naver.com>
2022-05-09 16:57:10 +0900

    study: tableView 생성 및 정적 데이터 출력

--- UIKit/Contacts/Contacts/ViewController.swift
+++ UIKit/Contacts/Contacts/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+
+    let names = ["Jaehoon", "Jaehyun", "Bongho"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // UITableViewDelegate 와 UITableDataSource 를 이용하여 메소드 구현 (Delegate Pattern)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    // cell item 터치 이벤트
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped item")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    // section 개수
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    // row 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+
+    // cell 지정
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 계속 cell 을 재사용하여 렌더링하기 위함이라고 생각됨
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // indexPath 를 이용해 1:1 매핑 가능
+        cell.textLabel?.text = names[indexPath.row]
+
+        return cell
+    }
+}
