bff517bb9f6010cb819754f0140e2ced941080f0
trumanfromkorea <jangjh971008@naver.com>
2022-06-14 02:14:16 +0900

    add: alamofire

--- UIKit/Anthill/Anthill/SignUpViewController.swift
+++ UIKit/Anthill/Anthill/SignUpViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-    
     static let identifier = "SignUpViewController"
     static let storyboard = "SignUp"
 
-    @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var verifyPasswordTextField: UITextField!
-    @IBOutlet weak var nicknameTextField: UITextField!
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var phoneNumberTextField: UITextField!
-    
+    @IBOutlet var idTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var verifyPasswordTextField: UITextField!
+    @IBOutlet var nicknameTextField: UITextField!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var phoneNumberTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+
     @IBAction func onTappedDoneButton(_ sender: Any) {
+        signUpWithUserInfo()
     }
     
+    func signUpWithUserInfo() {
+        
+    }
 }
