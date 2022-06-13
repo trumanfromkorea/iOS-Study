ed2c5f04d3e546252efe28ee4921e9158ae2faa0
trumanfromkorea <jangjh971008@naver.com>
2022-06-14 02:31:31 +0900

    test: with api server - signup

--- UIKit/Anthill/Anthill/ViewController.swift
+++ UIKit/Anthill/Anthill/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func navigateToSignUp(_ sender: Any) {
+        let storyboard = UIStoryboard(name: SignUpViewController.storyboard, bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: SignUpViewController.identifier) as! SignUpViewController
 
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
-
