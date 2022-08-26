d41e75cb656c2efe746ab13434b79cefed0a4119
trumanfromkorea <jangjh971008@naver.com>
2022-08-26 17:07:43 +0900

    create chatui study project

---
+++ UIKit/ChatUI/ChatUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatUI
+//
+//  Created by 장재훈 on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
